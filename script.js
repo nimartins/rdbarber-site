d274e8b6ac202e1aee62602cf9410af0c311f89f
Nicole <nicpmartins@gmail.com>
2025-06-15 15:33:00 -0300

    Ajuste na view do iframe

--- script.js
+++ script.js
@@ -7,13 +7,23 @@ const headerItemsToHide = [
   document.querySelector('.bio')
 ];
 
+const isMobile = window.innerWidth < 768;
+
 tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
+    const targetId = tab.getAttribute("data-tab");
+
+    // Redireciona diretamente para o site de agendamento se for mobile
+    if (isMobile && targetId === "agendamento") {
+      window.open("https://sites.appbarber.com.br/barbeariaaraujo-ryjr", "_blank");
+      return;
+    }
+
+    // Normal navegação por abas
     tabs.forEach((btn) => btn.classList.remove("active"));
     contents.forEach((content) => content.classList.remove("active"));
 
     tab.classList.add("active");
-    const targetId = tab.getAttribute("data-tab");
     document.getElementById(targetId).classList.add("active");
 
     if (targetId === "agendamento") {
