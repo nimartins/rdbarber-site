3dab52ce47a46529fb757cea2927c96d4542e2f7
Nicole <nicpmartins@gmail.com>
2025-06-15 15:38:24 -0300

    Removido o frame do agendamento

--- script.js
+++ script.js
@@ -13,12 +13,6 @@ tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
     const targetId = tab.getAttribute("data-tab");
 
-    // Redireciona diretamente para o site de agendamento se for mobile
-    if (isMobile && targetId === "agendamento") {
-      window.open("https://sites.appbarber.com.br/barbeariaaraujo-ryjr", "_blank");
-      return;
-    }
-
     // Normal navegação por abas
     tabs.forEach((btn) => btn.classList.remove("active"));
     contents.forEach((content) => content.classList.remove("active"));
@@ -26,10 +20,5 @@ tabs.forEach((tab) => {
     tab.classList.add("active");
     document.getElementById(targetId).classList.add("active");
 
-    if (targetId === "agendamento") {
-      headerItemsToHide.forEach(item => item.classList.add('header-item-hidden'));
-    } else {
-      headerItemsToHide.forEach(item => item.classList.remove('header-item-hidden'));
-    }
   });
 });
